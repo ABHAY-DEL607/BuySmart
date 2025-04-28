7082e034e84c985a57e8813361d2b054664ba986
ABHAY-DEL607 <abhaythakur98453@gmail.com>
2025-04-28 15:41:54 +0530

    aaa

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,4 +1,51 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        remotePatterns: [
+          {
+            protocol: 'https',
+            hostname: 'drive.usercontent.google.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'images.pexels.com',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'upload.wikimedia.org',
+            port: '',
+            pathname: '/**',
+          },
+          {
+            protocol: 'https',
+            hostname: 'www.jiomart.com',
+            port: '',
+            pathname: '/**',
+          },
+         {
+          protocol: 'https',
+          hostname: 'images.unsplash.com',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'upload.wikimedia.org',
+          port: '',
+          pathname: '/**',
+        },
+        {
+          protocol: 'https',
+          hostname: 'www.jiomart.com',
+          port: '',
+          pathname: '/**',
+        },
+      ],
+    },
+  };
 
 export default nextConfig;
